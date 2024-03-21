4d2bfc0767369a733520db8e7afa0a502a72cde4
Varun Gore <varungor@amazon.com>
2024-03-21 15:23:59 -0700

    Add sample Lambda handler to putJobSuccessResult

---
+++ codepipeline/LambdaInvokeActionHandler.js
@@ -0,0 +1,58 @@
+var AWS = require('aws-sdk');
+
+exports.handler = async (event, context) => {
+    var codepipeline = new AWS.CodePipeline();
+
+    // Retrieve the Job ID from the Lambda action
+    var jobId = event["CodePipeline.job"].id;
+
+    // Retrieve the value of UserParameters from the Lambda action configuration in CodePipeline, in this case a URL which will be
+    // health checked by this function.
+    var url = event["CodePipeline.job"].data.actionConfiguration.configuration.UserParameters;
+
+    // Custom logic to perform
+    // ...
+
+    // Notify CodePipeline of a successful job
+    var putJobSuccess = function (message) {
+        var params = {
+            jobId: jobId,
+            executionDetails: {
+                summary: JSON.stringify(message),
+                externalExecutionId: context.awsRequestId
+            },
+            outputVariables: {
+                awsRequestId: context.awsRequestId
+            }
+        };
+
+        return codepipeline.putJobSuccessResult(params, function (err, data) {
+            if (err) {
+                context.fail(err);
+            } else {
+                context.succeed(message);
+            }
+        }).promise();
+    };
+
+    // Notify CodePipeline of a failed job
+    var putJobFailure = function (message) {
+        var params = {
+            jobId: jobId,
+            failureDetails: {
+                message: JSON.stringify(message),
+                type: 'JobFailed',
+                externalExecutionId: context.awsRequestId
+            }
+        };
+        return codepipeline.putJobFailureResult(params, function (err, data) {
+            if (err) {
+                context.fail(message);
+            }
+        }).promise();
+    };
+
+    var response = await putJobSuccess("Tests passed.");
+
+    return response;
+};
